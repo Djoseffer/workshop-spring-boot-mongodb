03fc6c65240aae448d4134c37cbdbaf506062ee2
Djoseffer <tom_djosefer@hotmail.com>
2023-09-23 12:36:13 -0300

    insert put, delete and post

--- src/main/java/com/aula/workshopmongo/resource/UserResource.java
+++ src/main/java/com/aula/workshopmongo/resource/UserResource.java
@@ -6,9 +6,10 @@ import com.aula.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "users")
@@ -29,4 +30,27 @@ public class UserResource {
       User obj = service.findById(id);
         return ResponseEntity.ok().body(new UserDTO(obj));
     }
+
+ @PostMapping
+  public ResponseEntity <Void> insert(@RequestBody UserDTO objDTO) {
+    User obj = service.fromDTO(objDTO);
+    obj = service.insert(obj);
+    URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+    return ResponseEntity.created(uri).build();
+  }
+
+  @RequestMapping(value = "/{id}",method = RequestMethod.DELETE)
+  public ResponseEntity <Void> delete(@PathVariable String id) {
+    service.delete(id);
+    return ResponseEntity.noContent().build();
+  }
+
+  @RequestMapping(value = "/{id}",method = RequestMethod.PUT)
+  public ResponseEntity <Void> update(@RequestBody UserDTO objDTO, @PathVariable String id) {
+    User obj = service.fromDTO(objDTO);
+    obj.setId(id);
+    obj = service.update(obj);
+    return ResponseEntity.noContent().build();
+  }
+
 }
--- src/main/java/com/aula/workshopmongo/services/UserService.java
+++ src/main/java/com/aula/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.aula.workshopmongo.services;
 
 import com.aula.workshopmongo.domain.User;
+import com.aula.workshopmongo.dto.UserDTO;
 import com.aula.workshopmongo.services.exception.ObjectNotFounException;
 import com.aula.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,4 +28,29 @@ public class UserService {
         return userOptional.orElseThrow(() -> new ObjectNotFounException("Object not found"));
     }
 
+    public User insert(User obj){
+        return repo.insert(obj);
+    }
+
+    public void delete(String id){
+        findById(id);
+        repo.deleteById(id);
+    }
+
+    public User update(User obj) {
+        Optional<User> userOptional = repo.findById(obj.getId());
+        User newObj = userOptional.orElseThrow(() -> new ObjectNotFounException("Object not found"));
+        updateData(newObj, obj);
+        return repo.save(newObj);
+    }
+
+    private void updateData(User newObj, User obj) {
+        newObj.setName(obj.getName());
+        newObj.setEmail(obj.getEmail());
+
+    }
+
+    public User fromDTO(UserDTO objDTO){
+        return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
+    }
 }
